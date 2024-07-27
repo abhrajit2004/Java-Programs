38c385a20c52785e62963e3341ff29b6aca9e1eb
abhrajit2004 <abhrajitgupta.it2022@nsec.ac.in>
2024-07-27 16:23:45 +0530

    second commit

---
+++ src/PracticeSet2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class PracticeSet2 {
+    public static void main(String[] args) {
+//        float a = (7/4f) * (9/2f);
+//        System.out.println(a);
+
+//        char grade = 'B';
+//        grade = (char)(grade + 8);
+//        System.out.println(grade);
+//        Decrypting the grade
+//        grade = (char)(grade - 8);
+//        System.out.println(grade);
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter a number: ");
+//        int user = sc.nextInt();
+//        int given_number = 20;
+//
+//        if(given_number > user){
+//            System.out.println("Given number is greater");
+//        }
+//        else{
+//            System.out.println("Given number is not greater");
+//        }
+
+//        (v*v-u*u)/2*a*s
+    }
+}
