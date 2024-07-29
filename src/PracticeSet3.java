293316368441999e235cb3eec43bd60b679e3068
abhrajit2004 <abhrajitgupta.it2022@nsec.ac.in>
2024-07-29 21:10:46 +0530

    committed new files

---
+++ src/PracticeSet3.java
@@ -0,0 +1,31 @@
+public class PracticeSet3 {
+    public static void main(String[] args) {
+//        1. Convert to lowercase
+//        String a = "Frank Castle";
+//        a = a.toLowerCase();
+//        System.out.println(a);
+
+//        2. Replace spaces with underscore
+//        String sentence = "I am a good boy";
+//        sentence = sentence.replace(" ","_");
+//        System.out.println(sentence);
+
+//        3. Fill in a letter template
+//        String letter = "Dear <|name|>, Thanks a lot";
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter your name: ");
+//        String name = sc.nextLine();
+//        letter = letter.replace("<|name|>", name);
+//        System.out.println(letter);
+
+//        4. Detect double and triple spaces in a string
+//        String text = "This text has  double space and  triple space";
+//        System.out.println(text.indexOf("  "));
+//        System.out.println(text.indexOf("   "));
+
+//        5. Formatting letter using escape sequence characters
+        String letter = "Dear Abhra,\n\tThis Java Course is nice.\n\tThanks";
+        System.out.println(letter);
+
+    }
+}
---
+++ src/PracticeSet4.java
@@ -0,0 +1,87 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
+public class PracticeSet4 {
+    public static void main(String[] args) {
+//        int a = 10;
+//        if(a = 11){
+//            System.out.println("I am 11");
+//        }
+//        else{
+//            System.out.println("I am not 11");
+//        }
+
+//        byte sub1, sub2, sub3;
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter marks of Physics: ");
+//        sub1 = sc.nextByte();
+//        System.out.println("Enter marks of Chemistry: ");
+//        sub2 = sc.nextByte();
+//        System.out.println("Enter marks of Mathematics: ");
+//        sub3 = sc.nextByte();
+//        float avg = (sub1 +  sub2 + sub3)/3f;
+//        if(avg >= 40 && sub1 >= 33 && sub2 >= 33 && sub3 >= 33){
+//            System.out.println("Pass! You have got percentage of " + avg + " in total");
+//        }
+//        else{
+//            System.out.println("Fail! You have got percentage of " + avg + " in total");
+//        }
+
+//        float tax = 0;
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter your income in lakhs per annum: ");
+//        float income = sc.nextFloat();
+//        if(income <= 2.5f){
+//            tax = tax + 0;
+//        }
+//        else if(income>2.5f && income<=5f){
+//            tax = tax + 0.05f *(income - 2.5f);
+//        }
+//        else if(income>5f && income <=10.0f){
+//            tax = tax + 0.05f *(5.0f - 2.5f);
+//            tax = tax + 0.2f *(income - 5.0f);
+//        }
+//        else if(income>10.0f){
+//            tax = tax + 0.05f *(5.0f - 2.5f);
+//            tax = tax + 0.2f *(10.0f - 5.0f);
+//            tax = tax + 0.3f *(income - 10.0f);
+//        }
+//
+//        System.out.println("The total tax paid by the employee is: " + tax);
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter the day number: ");
+//        byte day_number = sc.nextByte();
+//        switch (day_number){
+//            case 1-> System.out.println("Monday");
+//            case 2-> System.out.println("Tuesday");
+//            case 3-> System.out.println("Wednesday");
+//            case 4-> System.out.println("Thursday");
+//            case 5-> System.out.println("Friday");
+//            case 6-> System.out.println("Saturday");
+//            case 7-> System.out.println("Sunday");
+//        }
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter a year: ");
+//        int year = sc.nextInt();
+//        int check_leap_year = ((year%4==0 && year%100!=0) || year%400==0)?1:0;
+//        System.out.println(check_leap_year);
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter website URL: ");
+//        String website = sc.next();
+//        if(website.endsWith(".com")){
+//            System.out.println("This is a Commercial website");
+//        }
+//        else if (website.endsWith(".org")) {
+//            System.out.println("This is an Organizational website");
+//        }
+//        else if(website.endsWith(".in")) {
+//            System.out.println("This is an Indian website");
+//        }
+
+    }
+}
+
